5f6eb6386a3170b02db87fa1979185e93e5fdf41
Mamadou2025A <sowk78824@gmail.com>
2026-01-21 20:27:59 -0500

    Fix i18n imports

--- src/i18n.js
+++ src/i18n.js
@@ -2,12 +2,12 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
-import en from './en.json'
-import fr from './fr.json'
-import es from './es.json'
-import ar from './ar.json'
-import pt from './pt.json'
-import zh from './zh.json'
+import en from './locales/en.json'
+import fr from './locales/fr.json'
+import es from './locales/es.json'
+import ar from './locales/ar.json'
+import pt from './locales/pt.json'
+import zh from './locales/zh.json'
 
 i18n
   .use(LanguageDetector)
